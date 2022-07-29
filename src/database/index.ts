5b2e6017d08177babd13da39480ab3a04bf28707
MD Shafiul Islam <shafiul2014bd@gmail.com>
2022-07-29 07:09:04 +0600

    Fix DB Config

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,17 +1,18 @@
+import { join } from "path";
 import { DataSourceOptions } from "typeorm";
 
 export const dbConnectionOption = ():DataSourceOptions => {
   // console.log("__dirname, ", __dirname + "/../model/*{.js,.ts}");
   return {
     type: "mysql",
-    host: "localhost",
+    host: "venus.mydchub.com",
     port: 3306,
-    username: "med_shafiul",
-    password: "01933408421S",
-    database: "med",
+    username: "mediecol_med_shafiul",
+    password: "oRV)e(2,+X$K",
+    database: "mediecol_bc_bd",
     logging: false,
     synchronize: true,
-    entities: [__dirname + "/../model/*{.js,.ts}"],
+    entities: [join(__dirname, "..", "model", "*{.js,.ts}")],
   };
 };
 
